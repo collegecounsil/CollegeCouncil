cf9988742cb65b0b51405dadb57c6d849703ffe6
Raj Chauhan <collegecounsil@gmail.com>
2018-02-04 23:35:40 +0530

    Image Url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,9 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent
   ],
-  providers: [],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
